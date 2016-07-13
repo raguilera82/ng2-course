30857ae877ffe0fde16f5e5ebcb57511a46e7667
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 22:14:01 +0200

    Add button to go to Hello from other component through the router

--- app/other/other.component.ts
+++ app/other/other.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { RouteSegment } from '@angular/router'
+import { RouteSegment, Router } from '@angular/router'
 
 @Component({
   selector: 'other',
   template: `<p>Parameters:</p>
              <p>Param 1: {{param1}}</p>
-             <p>Param 2: {{param2}}</p>`
+             <p>Param 2: {{param2}}</p>
+             <button (click)="goToHello()">Go to Hello</button>`
 })
 
 export class OtherComponent implements OnInit{
@@ -13,11 +14,16 @@ export class OtherComponent implements OnInit{
   param1: string
   param2: string
 
-  constructor(private routeSegment:RouteSegment){}
+  constructor(private routeSegment:RouteSegment,
+              private router:Router){}
 
   ngOnInit(){
       this.param1 = this.routeSegment.getParam('param1')
       this.param2 = this.routeSegment.getParam('param2')
+  }
+
+  goToHello(){
+    this.router.navigate(['/hello'])
   }
 
 }
