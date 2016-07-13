c24bc5e8f76b230f11af3bdc93784b055ab02d76
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 20:27:56 +0200

    Using configuration data in example service

--- app/example/example.service.ts
+++ app/example/example.service.ts
@@ -1,7 +1,13 @@
+import {Inject} from '@angular/core'
+
+import {CONFIG_TOKEN, Configuration} from '../config/configuration'
+
 export class ExampleService {
 
+  constructor(@Inject(CONFIG_TOKEN) private config:Configuration){}
+
   getExample(): string {
-    return 'This is an example'
+    return 'This is an example ' + this.config.api
   }
 
 }
