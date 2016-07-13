c48a5cd4860daf7bda04b4fb3c6a8a202b092651
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 21:47:34 +0200

    Defition routes in main component before error missing base href

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,20 @@
 import {Component} from '@angular/core'
+import { Routes, ROUTER_DIRECTIVES } from '@angular/router'
+
 import {HelloWorldComponent} from './helloworld/hello-world.component'
 
 @Component({
   selector: 'ng2-app',
-  template: `<ng2-hello-world></ng2-hello-world>`,
-  directives: [HelloWorldComponent]
+  template: `<nav>
+                <a [routerLink]="['/hello']">Hello</a>
+             </nav>
+             <router-outlet></router-outlet>`,
+  directives: [ROUTER_DIRECTIVES]
 })
+
+@Routes([
+  {path: '/hello', component: HelloWorldComponent}
+])
 
 export class AppComponent{
 
