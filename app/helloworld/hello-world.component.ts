adb7acee423fd73f412046eae4b3b8769713e0b1
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 22:17:33 +0200

    Implemented go to other with params through the router

--- app/helloworld/hello-world.component.ts
+++ app/helloworld/hello-world.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 import { HelloWorldService } from './hello-world.service'
 
 @Component({
   selector: 'ng2-hello-world',
   providers: [HelloWorldService],
-  template: `<p id="hello">{{hello}}</p>`
+  template: `<p id="hello">{{hello}}</p>
+             <button (click)="goToOther()">Go to other</button>`
 
 })
 
 export class HelloWorldComponent implements OnInit{
   hello: string
 
-  constructor(private helloWorldService:HelloWorldService){
+  constructor(private helloWorldService:HelloWorldService,
+              private router:Router){
   }
 
   ngOnInit(){
     this.hello = this.helloWorldService.getHello()
+  }
+
+  goToOther(){
+    this.router.navigate(['/other', 'from', 'hello'])
   }
 
 }
