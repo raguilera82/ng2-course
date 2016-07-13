889baefeed431e775a252647d1e839c97bcf497b
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 18:48:03 +0200

    Event binding example with click button

--- app/example/data-example.component.ts
+++ app/example/data-example.component.ts
@@ -3,7 +3,8 @@ import {Component, OnInit} from '@angular/core'
 @Component({
   selector: 'data-example',
   template: `<p>{{name}}</p>
-              <p [innerHTML]="name"></p>`
+             <p [innerHTML]="name"></p>
+             <button (click)="onClick()">Click</button>  `
 })
 
 export class DataExampleComponent implements OnInit {
@@ -12,6 +13,10 @@ export class DataExampleComponent implements OnInit {
 
   ngOnInit(){
     this.name = 'Mateo'
+  }
+
+  onClick(){
+    console.log('Clicked button')
   }
 
 }
