f6297fdf25236723f48583f34b51a916c76a5735
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 20:01:46 +0200

    Error because ExampleService no provider

--- app/helloworld/hello-world.service.ts
+++ app/helloworld/hello-world.service.ts
@@ -1,11 +1,14 @@
+import {Injectable} from '@angular/core'
+
 import {ExampleService} from '../example/example.service'
 
+@Injectable()
 export class HelloWorldService{
 
   constructor(private service:ExampleService){
 
   }
-  
+
   getHello():string{
     return 'Hello World! ' + this.service.getExample()
   }
