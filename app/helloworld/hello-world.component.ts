ec0f3b9f24d97f00e19b74e6d745be337cc3359d
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 20:04:01 +0200

    Add ExampleService as provider

--- app/helloworld/hello-world.component.ts
+++ app/helloworld/hello-world.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 
 import { HelloWorldService } from './hello-world.service'
+import { ExampleService } from '../example/example.service'
 
 @Component({
   selector: 'ng2-hello-world',
-  providers: [HelloWorldService],
+  providers: [HelloWorldService, ExampleService],
   template: `<p id="hello">{{hello}}</p>`
 
 })
