fcc286ffcb7c8a4601a09a3b0caaeab946c98929
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 21:42:18 +0200

    Enable router configuration

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -5,6 +5,12 @@ import 'rxjs'
 import 'zone.js/dist/zone'
 import 'zone.js/dist/long-stack-trace-zone'
 import { bootstrap } from '@angular/platform-browser-dynamic'
+import { ROUTER_PROVIDERS } from '@angular/router'
+import { LocationStrategy, PathLocationStrategy } from '@angular/common'
+import { provide } from '@angular/core'
 import {AppComponent} from './main'
 
-bootstrap(AppComponent)
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: PathLocationStrategy})
+])
