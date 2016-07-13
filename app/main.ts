e4d9fec20d6801c239e6fe87d956eaadccfa0bc8
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 21:56:04 +0200

    Add other component route and link

--- app/main.ts
+++ app/main.ts
@@ -2,18 +2,21 @@ import {Component} from '@angular/core'
 import { Routes, ROUTER_DIRECTIVES } from '@angular/router'
 
 import {HelloWorldComponent} from './helloworld/hello-world.component'
+import {OtherComponent} from './other/other.component'
 
 @Component({
   selector: 'ng2-app',
   template: `<nav>
                 <a [routerLink]="['/hello']">Hello</a>
+                <a [routerLink]="['/other']">Other</a>
              </nav>
              <router-outlet></router-outlet>`,
   directives: [ROUTER_DIRECTIVES]
 })
 
 @Routes([
-  {path: '/hello', component: HelloWorldComponent}
+  {path: '/hello', component: HelloWorldComponent},
+  {path: '/other', component: OtherComponent}
 ])
 
 export class AppComponent{
