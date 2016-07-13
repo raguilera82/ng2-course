d75b0036fa19cca74296f9de7dc033f448e1ffcd
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-13 19:45:05 +0200

    HelloService error because does not use decorator Injectable

--- app/helloworld/hello-world.service.ts
+++ app/helloworld/hello-world.service.ts
@@ -1,6 +1,12 @@
+import {ExampleService} from '../example/example.service'
 
 export class HelloWorldService{
+
+  constructor(private service:ExampleService){
+
+  }
+  
   getHello():string{
-    return 'Hello World!'
+    return 'Hello World! ' + this.service.getExample()
   }
 }
