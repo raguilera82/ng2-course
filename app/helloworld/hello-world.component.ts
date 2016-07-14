c8962998a0a16b37833ac88245d00f5fe4991d8c
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-14 07:09:09 +0200

    Retrieve fake quote

--- app/helloworld/hello-world.component.ts
+++ app/helloworld/hello-world.component.ts
@@ -5,18 +5,25 @@ import { HelloWorldService } from './hello-world.service'
 @Component({
   selector: 'ng2-hello-world',
   providers: [HelloWorldService],
-  template: `<p id="hello">{{hello}}</p>`
+  template: `<p id="hello">{{hello}}</p>
+             <button (click)="getQuote()">Get Quote</button>
+             <p>{{quote}}</p>`
 
 })
 
 export class HelloWorldComponent implements OnInit{
   hello: string
+  quote: string
 
   constructor(private helloWorldService:HelloWorldService){
   }
 
   ngOnInit(){
     this.hello = this.helloWorldService.getHello()
+  }
+
+  getQuote(){
+    this.quote = 'Quote Fake'
   }
 
 }
