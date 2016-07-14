2b8bdc3b1c7c76f8234375402bff09cd18307b1e
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-14 07:05:32 +0200

    Add universal configuration

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -6,8 +6,12 @@ import 'zone.js/dist/zone'
 import 'zone.js/dist/long-stack-trace-zone'
 import { bootstrap } from '@angular/platform-browser-dynamic'
 import { HTTP_PROVIDERS } from '@angular/http'
-import {AppComponent} from './main'
+import { provide } from '@angular/core'
+
+import { CONFIG_TOKEN, config } from './config/configuration'
+import {AppComponent} from './main' 
 
 bootstrap(AppComponent, [
-  HTTP_PROVIDERS
+  HTTP_PROVIDERS,
+  provide(CONFIG_TOKEN, {useValue: config})
 ])
