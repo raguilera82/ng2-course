ea23c8de8e5b1790d92e80e9ff13b0d5f1e64b53
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-07-14 06:58:55 +0200

    Enable HTTP providers

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -5,6 +5,9 @@ import 'rxjs'
 import 'zone.js/dist/zone'
 import 'zone.js/dist/long-stack-trace-zone'
 import { bootstrap } from '@angular/platform-browser-dynamic'
+import { HTTP_PROVIDERS } from '@angular/http'
 import {AppComponent} from './main'
 
-bootstrap(AppComponent)
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS
+])
