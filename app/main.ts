5e514c9a53fa229d1a2e7ebad4c2b728a5b33739
raguilera <raguilera@autentia.com>
2016-08-11 09:40:02 +0200

    Back to the good

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core'
 import {SearchUserComponent} from './search-user/search-user.component'
-import {SearchUserComponentBad} from './search-user/search-user-bad.component'
 
 @Component({
   selector: 'ng2-app',
-  template: `<au-search-user-bad></au-search-user-bad>`,
-  directives: [SearchUserComponentBad]
+  template: `<au-search-user></au-search-user>`,
+  directives: [SearchUserComponent]
 })
 
 export class AppComponent{
